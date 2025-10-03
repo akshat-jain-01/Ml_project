f92abac96b5b3e21724c453de9ef4ed6a8ffb80f
akshat-jain-01 <akshatjainkht01@gmail.com>
2025-10-03 22:41:01 +0530

    mlflow

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ##End to end Datascience project
+
+import dagshub
+dagshub.init(repo_owner='akshatjainkht01', repo_name='Ml_project', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -16,96 +16,120 @@ from sklearn.model_selection import RandomizedSearchCV
 from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
 
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 
 from src.mlproject.utils import save_object, evaluate_models
+import dagshub
+
+dagshub.init(repo_owner='akshatjainkht01', repo_name='Ml_project', mlflow=True)
+
 
 @dataclass
 class ModelTrainerConfig():
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
 
 class ModelTrainer():
 
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
     def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Split train test input data")
 
             x_train, y_train, x_test, y_test = (
-                train_array[:,:-1],
+                train_array[:, :-1],
                 train_array[:, -1],
-                test_array[:,:-1],
+                test_array[:, :-1],
                 test_array[:, -1]
             )
 
             models = {
-                "Random Forest" : RandomForestRegressor(),
-                "Decision Tree" : DecisionTreeRegressor(),
-                "Gradient Boosting" : GradientBoostingRegressor(),
-                "Linear Regression" : LinearRegression(),
-                "XGBRegressor" : XGBRegressor(),
-                "CatBoosting Regressor" : CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor" : AdaBoostRegressor()
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
             }
 
-            params={
-                "Decision Tree" : {
-                    'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    #'splitter' : ['best', 'random'],
-                    # 'max_features' : ['sqrt', 'log2'],
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                 },
-
-                "Random Forest" : {
-                    #'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    #'max_features' : ['sqrt', 'log2', None],
-                    'n_estimators' : [8,16,32,64,128,256]
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-
-                "Gradient Boosting" : {
-                    #'loss' : ['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate' : [.1, .01, .05, .001],
-                    'subsample' : [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
-                    #'criterion' : ['squared_error', 'friedman_mse'],
-                    #'max_features' : [''auto', sqrt', 'log2'],
-                    'n_estimators' : [8,16,32,64,128,256]
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-
-                "Linear Regression" : {},
-
-                "XGBRegressor" : {
-                    'learning_rate' : [.1, .01, .05, .001],
-                    'n_estimators' : [8,16,32,64,128,256]
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-
-                "CatBoosting Regressor" : {
-                    'depth' : [6,8,10],
-                    'learning_rate' : [.1, .01, .05, .001],
-                    'iterations' : [30,50,100]
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [.1, .01, .05, .001],
+                    'iterations': [30, 50, 100]
                 },
-
-                "AdaBoost Regressor" : {
-                    'learning_rate' : [.1, .01, .05, .001],
-                    #'loss' : ['linear', 'square', 'exponential'],
-                    'n_estimators' : [8,16,32,64,128,256]
-                } 
-
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
             }
 
-            model_report : dict= evaluate_models(x_train, y_train, x_test, y_test, models, params)
+            model_report: dict = evaluate_models(x_train, y_train, x_test, y_test, models, params)
 
             best_model_score = max(sorted(model_report.values()))
-
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             best_model = models[best_model_name]
+
+            print("This is the best model")
+            print(best_model_name)
+
+            best_params = params.get(best_model_name, {})
+
+            mlflow.set_tracking_uri("https://dagshub.com/akshatjainkht01/Ml_project.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(x_test)
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+                # -------------------- FIX FOR DAGSHUB --------------------
+                # Original code that caused error:
+                # mlflow.sklearn.log_model(best_model, "model")
+                
+                # Fix: Save model locally and log as artifact to DagsHub
+                mlflow.sklearn.save_model(best_model, "artifacts/model")  # save locally
+                mlflow.log_artifacts("artifacts/model", artifact_path="model")  # log to DagsHub
+                # -----------------------------------------------------------
 
             if best_model_score < 0.6:
                 raise CustomException("No best model found")
-            
+
             logging.info(f"Best found model on training and testing dataset")
 
             save_object(
@@ -114,10 +138,8 @@ class ModelTrainer():
             )
 
             predicted = best_model.predict(x_test)
-
             r2_square = r2_score(y_test, predicted)
             return r2_square
-            
 
         except Exception as e:
             raise CustomException(e, sys)
