b16a80169546263d01ac1f005d7d7c2e9438c0db
akshat-jain-01 <akshatjainkht01@gmail.com>
2025-10-03 20:04:33 +0530

    trained model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion, DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig, ModelTrainer
 import sys
 
 if __name__=="__main__":
@@ -14,7 +15,10 @@ if __name__=="__main__":
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
 
         data_transformation = DataTransformation()
-        data_transformation.initaite_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr,_ = data_transformation.initaite_data_transformation(train_data_path, test_data_path)
+
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
     except Exception as e:
         logging.exception("An error occurred during execution")
